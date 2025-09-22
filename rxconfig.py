64c7da94f1c2771ff1049a9613e4d321094dda0a
Julitown46 <morenojulian4502@gmail.com>
2025-09-22 15:33:58 +0200

    New build No Websocket

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="PersonalWeb",
     frontend_only=True,
+    websocket_url=None,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
