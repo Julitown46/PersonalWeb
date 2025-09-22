d7314cbbfeaf270ed66f8d2d03eddff25034cf4e
Julitown46 <morenojulian4502@gmail.com>
2025-09-22 15:21:25 +0200

    New deploy with frontend only

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="PersonalWeb",
+    frontend_only=True,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
